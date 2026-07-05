0e04f5c9ec9dd2a684abcee2aa77c418a23e1a4e
Zachian <nanjimenwai41@Gmail.com>
2026-07-05 09:52:55 +0800

    v1.3.2 修复UI空行问题 增加已激活应用置顶

--- app/src/main/java/com/fuck/snooping/MainActivity.java
+++ app/src/main/java/com/fuck/snooping/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
